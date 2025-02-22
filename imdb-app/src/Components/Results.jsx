d84938de8bf5fde059024e56ec6f61f6241a024a
PreetS123 <preetbhardwaj517@gmail.com>
2025-02-22 16:31:14 +0530

    card created

--- imdb-app/src/Components/Results.jsx
+++ imdb-app/src/Components/Results.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from "react";
+import Card from "./common/Card";
 
-const Results = ({results}) => {
+const Results = ({ results }) => {
   return (
-    <div>
-      {results?.map((ele,i)=>(
-        <div>
-        <h1>{ele.original_title}</h1></div>
+    <div 
+    className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3 mx-w-6xl m-auto">
+      {results?.map((ele, i) => (
+        <div key={i}>
+          <Card singleCardEle={ele} key={i} />
+        </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Results
+export default Results;
