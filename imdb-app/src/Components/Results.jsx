026cd87d58bd40424b1d793c062ad28584ddc7d4
PreetS123 <preetbhardwaj517@gmail.com>
2025-02-22 12:46:35 +0530

    fetched data imdb database

---
+++ imdb-app/src/Components/Results.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Results = ({results}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Results
--- imdb-app/src/app/page.js
+++ imdb-app/src/app/page.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import Results from "@/Components/Results";
 
-const Home = () => {
-  return (
-    <div>
-      <h1>My Home page.</h1>
-    </div>
-  )
-}
+const API_KEY = process.env.API_KEY;
+
+const Home = async ({ searchParam }) => {
+  const genre = searchParam?.genre || 'fetchTrending';
+
+  const url = `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`;
+
+  try {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      const errorText = await res.text(); // Log raw response in case of an error
+      console.error("API Error Response:", errorText);
+      throw new Error('Failed to fetch');
+    }
+
+    const data = await res.json();
+    const results = data?.results || [];
+
+    console.log(results);
+
+    return (
+      <div>
+        <Results results={results} />
+      </div>
+    );
+  } catch (error) {
+    console.error("Fetch Error:", error);
+    return <div>Error loading data</div>;
+  }
+};
 
 export default Home;
