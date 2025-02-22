93f8e35171411808344b0794a989e997273868c4
PreetS123 <preetbhardwaj517@gmail.com>
2025-02-22 11:13:25 +0530

    about section created

---
+++ imdb-app/src/app/about/page.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-3">
+      <h1 className="text-2xl text-amber-600 font-medium ">About</h1>
+      <p>
+        The Internet Movie Database (IMDb) is an online database of information
+        related to films, television series, podcasts, home videos, video games,
+        and streaming content online – including cast, production crew and
+        personal biographies, plot summaries, trivia, ratings, and fan and
+        critical reviews. IMDb began as a fan-operated movie database on the
+        Usenet group &quot;rec.arts.movies&quot; in 1990, and moved to the Web
+        in 1993. Since 1998, it has been owned and operated by IMDb.com, Inc., a
+        subsidiary of Amazon.
+      </p>
+      <p>
+        The site&apos;s message boards were disabled in February 2017. As of
+        2024, IMDb was the 51st most visited website on the Internet, as ranked
+        by Semrush.As of March 2022, the database contained some 10.1
+        million titles (including television episodes), 11.5 million person
+        records, and 83 million registered users.
+      </p>
+      <p>
+        The title and talent pages of IMDb are accessible to all users, but only
+        registered and logged-in users can submit new material and suggest edits
+        to existing entries. Most of the site&apos;s data has been provided by
+        these volunteers. Registered users with a proven track record are able
+        to add and make corrections to cast lists, credits, and some other data
+        points. However, the addition and removal of images, and alterations to
+        titles, cast and crew names, character names, and plot summaries are
+        subject to an approval process; this usually takes between 24 and 72
+        hours.
+      </p>
+    </div>
+  );
+};
+
+export default About;
