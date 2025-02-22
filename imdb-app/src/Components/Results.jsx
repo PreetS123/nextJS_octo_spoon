892cf103065b0890d5f9f863f8a31a5e6346aab0
PreetS123 <preetbhardwaj517@gmail.com>
2025-02-22 12:52:13 +0530

    data mapped

--- imdb-app/src/Components/Results.jsx
+++ imdb-app/src/Components/Results.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Results = ({results}) => {
   return (
     <div>
-      
+      {results?.map((ele,i)=>(
+        <div>
+        <h1>{ele.original_title}</h1></div>
+      ))}
     </div>
   )
 }
