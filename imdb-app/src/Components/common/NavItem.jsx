83fbd46ac4dd21b4e0b14855ef98ced4e452bebe
PreetS123 <preetbhardwaj517@gmail.com>
2025-02-22 18:05:35 +0530

    deployment error fixed

--- imdb-app/src/Components/common/NavItem.jsx
+++ imdb-app/src/Components/common/NavItem.jsx
@@ -1,21 +1,28 @@
 "use client"
+import error from '@/app/error';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import React from 'react'
+import React, { Suspense } from 'react';
 
-const NavItem = ({title,param}) => {
+const NavItemContent = ({ title, param }) => {
+    const searchParams = useSearchParams();
+    const genre = searchParams.get('genre');
 
-    const searchParams= useSearchParams();
-    const genre= searchParams.get('genre');
-  return (
-    <>
-      <Link 
-      className={
-        `hover:text-amber-500 font-semibold
-        ${genre===param? 'undeline underline-offset-8 decoration-4 decoration-amber-400 rounded-lg':''}`}
-      href={`/?genre=${param}`}>{title}</Link>
-    </>
-  )
-}
+    return (
+        <Link 
+            className={`hover:text-amber-500 font-semibold
+            ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-400 rounded-lg' : ''}`}
+            href={`/?genre=${param}`}
+        >
+            {title}
+        </Link>
+    );
+};
+
+const NavItem = (props) => (
+    <Suspense fallback={error}>
+        <NavItemContent {...props} />
+    </Suspense>
+);
 
 export default NavItem;
